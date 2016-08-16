bfa6abdcbc47877e2b5434b64d759afed7f5da93
Stookey, Brandon <bstookey@quickbase.com>
2016-08-16 08:30:27 -0400

    create header

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,5 +1,6 @@
 const React = require('react')
 const { object } = React.PropTypes
+const Header = require('./Header')
 
 const Details = React.createClass({
   propTypes: {
@@ -10,12 +11,16 @@ const Details = React.createClass({
     const { title, description, year, poster, trailer } = params
     return (
       <div className='container'>
-        <header className='header'>
-          <h1 className='branding'>Stookey's Video</h1>
-        </header>
+        <Header />
         <div className='video-info'>
           <h1 className='video-title'>{title}</h1>
           <h2 className='video-year'>({year})</h2> 
+          <img className='video-poster' src={`public/img/posters/${poster}`} />
+          <p className='video-description'>{description}</p>
+        </div>
+        <div className='video-container'>
+          <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder=
+          '0' allowFullScreen></iframe>
         </div>
       </div>    
     )
